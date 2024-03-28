b05b31ffb88e17c99f57fbd673c339fc02b08d33
Mateusz Rajtar <mrajtar15@gmail.com>
2024-03-28 18:22:04 +0100

    feat: Add ImagePath column for movie posters upload

---
+++ ScreenSavvy.DataAccess/Migrations/20240328172115_AddImagePathColumnForMoviePosters.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScreenSavvy.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImagePathColumnForMoviePosters : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "MovieDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "MovieDetails");
+        }
+    }
+}
